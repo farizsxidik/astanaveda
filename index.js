ce4a902c97e8f527382605d2c469e99b5c4f1a76
Fariz Sidik <farizsidik@Farizs-MacBook-Pro-2.local>
2024-01-31 08:56:40 +0700

    ganti respone

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From BFLP IT" }');
+    res.send('{ "response": "Hello From Veteran" }');
 });
 
 app.get('/will', function (req, res) {
