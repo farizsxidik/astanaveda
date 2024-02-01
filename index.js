a525db535fc0c11a32468d87bca224fb59e28527
Fariz Sidik <farizsidik@Farizs-MacBook-Pro-2.local>
2024-02-01 16:01:33 +0700

    PAWTB-11 update commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From BFLP" }');
+    res.send('{ "response": "Hello From Fariz Sidik Muhamad" }');
 });
 
 app.get('/will', function (req, res) {
